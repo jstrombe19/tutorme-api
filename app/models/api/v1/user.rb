fd6aaae3e52e28b7f9ebfbade9ed219094323ed1
jstrombe19 <jaredstromberg@gmail.com>
2020-04-11 12:16:24 -0700

    Created the first database migration - User. Enabled CORS; added jwt, bcrypt, and rack-cors in gemfile; activated bcrypt in user model.

--- app/models/api/v1/user.rb
+++ app/models/api/v1/user.rb
@@ -1,2 +1,6 @@
 class Api::V1::User < ApplicationRecord
+  has_secure_password
+
+  
+
 end
--- db/migrate/20200411185748_create_api_v1_users.rb
+++ db/migrate/20200411185748_create_api_v1_users.rb
@@ -1,7 +1,13 @@
 class CreateApiV1Users < ActiveRecord::Migration[6.0]
   def change
     create_table :api_v1_users do |t|
-
+      t.string :name
+      t.string :username
+      t.string :email
+      t.integer :phone
+      t.string :password_digest
+      t.boolean :tutor
+      
       t.timestamps
     end
   end
